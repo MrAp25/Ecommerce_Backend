f2d857500e0242a4813d9768d966b6e0cf3d7fb9
MrAp25 <pankit281@gmail.com>
2021-05-17 16:32:27 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const cors = require('cors');
 const app = express();
+
+//Import Routes
+
+const orderRoute = require('./routes/order');
+const productsRoute = require('./routes/products');
+const usersRoute = require('./routes/users');
+
+//Use Routes
+app.use('/api/order',orderRoute);
+app.use('/api/products',productsRoute);
+app.use('/api/users',usersRoute);
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +28,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
